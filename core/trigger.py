c520aa1ee0d0236aff3a16c2e508a3b0b138fff9
Xiaoming Bai <baixiaoming555@qq.com>
2015-04-28 17:51:28 +0800

    实现了oplog中$set，$unset，以及普通update的触发

--- core/trigger.py
+++ core/trigger.py
@@ -28,29 +28,41 @@ class Trigger(BaseTrigger):
         if not tmp_dbs:
             return None
 
-        doc = message['msg']['o']['$set'] if '$set' in message['msg']['o'] else message['msg']['o']
-        if '_id' in doc:
-            doc.pop('_id')
-        op_id = message['msg']['o2']
-        trig_dbs = {}
+        # doc = message['msg']['o']['$set'] if '$set' in message['msg']['o'] else message['msg']['o']
+        doc_set = {} if '$set' not in message['msg']['o'] else message['msg']['o']['$set']
+        doc_unset = {} if '$unset' not in message['msg']['o'] else message['msg']['o']['$unset']
+        doc_update = {} if doc_set or doc_unset else message['msg']['o']
+        if '_id' in doc_update:
+            doc_update.pop('_id')
+        trig_dbs = {'update': {}, 'set': {}, 'unset': {}}
 
         for key in tmp_dbs:
             # attr_correspond将不包含_id
-            attr_correspond = filter(lambda x: True if x in doc.keys() else False, tmp_dbs[key].keys())
-            if attr_correspond:
-                trig_dbs[key] = {attr_correspond[i]: tmp_dbs[key][attr_correspond[i]] for i in
-                                 range(0, len(attr_correspond))}
-                trig_dbs[key]['_id']=tmp_dbs[key]['_id']
-        print "returnTriger:",trig_dbs
-        return trig_dbs
+            attr_set = filter(lambda x: True if x in doc_set.keys() else False, tmp_dbs[key].keys())
+            attr_unset = filter(lambda x: True if x in doc_unset.keys() else False, tmp_dbs[key].keys())
+            attr_update = filter(lambda x: True if x in doc_update.keys() else False, tmp_dbs[key].keys())
+            if attr_update:
+                trig_dbs['update'][key] = {attr_update[i]: tmp_dbs[key][attr_update[i]] for i in
+                                 range(0, len(attr_update))}
+                trig_dbs['update'][key]['_id'] = tmp_dbs[key]['_id']
+            if attr_set:
+                trig_dbs['set'][key] = {attr_set[i]: tmp_dbs[key][attr_set[i]] for i in
+                                 range(0, len(attr_set))}
+                trig_dbs['set'][key]['_id'] = tmp_dbs[key]['_id']
+            if attr_unset:
+                trig_dbs['unset'][key] = {attr_unset[i]: tmp_dbs[key][attr_unset[i]] for i in
+                                 range(0, len(attr_unset))}
+                trig_dbs['unset'][key]['_id'] = tmp_dbs[key]['_id']
 
+        print "returnTriger:", trig_dbs
+        return trig_dbs
 
 
         # print message
         # try:
         # if message['msg']['op'] != 'u':
         # print 'not update operation'
-        #         return None
+        # return None
         #     ns = message['msg']['ns']
         # except AttributeError:
         #     logging.info('message is invalid')
@@ -65,7 +77,7 @@ class Trigger(BaseTrigger):
         #     if key == ns:
         #         trig_dbs = db_correspond[key]
         #         break
-        #print 'update operation:', trig_dbs
+        # print 'update operation:', trig_dbs
         # return trig_dbs
 
     def process_message(self):
@@ -110,21 +122,49 @@ class Trigger(BaseTrigger):
         根据message，更新与其相对应的数据库
         """
         # print trig_dbs
-        doc = message['msg']['o']['$set'] if '$set' in message['msg']['o'] else message['msg']['o']
-        if '_id' in doc:
-            doc.pop('_id')  # 操作文档
+        doc_set = {} if '$set' not in message['msg']['o'] else message['msg']['o']['$set']
+        doc_unset = {} if '$unset' not in message['msg']['o'] else message['msg']['o']['$unset']
+        doc_update = {} if doc_set or doc_unset else message['msg']['o']
+        if '_id' in doc_update:
+            doc_update.pop('_id')  # 操作文档
         op_id = message['msg']['o2']['_id']
         # ns = msg['msg']['ns']  # 数据库.集合
-        for key in trig_dbs:
+        for key in trig_dbs['update']:
             db_collection = key.split('.')
             db_name = db_collection[0]
             collection_name = db_collection[1]
-            update_id=trig_dbs[key]['_id']
-            trig_dbs[key].pop('_id')
-            #update_dic = {attr.values()[0]: doc[attr.keys()[0]] for attr in trig_dbs[key]}
-            update_dic = {trig_dbs[key][attr]: doc[attr] for attr in trig_dbs[key]}
-            print update_id,':', op_id
-            print '$set:',update_dic
+            update_id = trig_dbs['update'][key]['_id']
+            trig_dbs['update'][key].pop('_id')
+            # update_dic = {attr.values()[0]: doc[attr.keys()[0]] for attr in trig_dbs[key]}
+            update_dic = {trig_dbs['update'][key][attr]: doc_update[attr] for attr in trig_dbs['update'][key]}
+            print update_id, ':', op_id
+            print key, ' update:', update_dic
+            col = get_mongodb(db_name, collection_name, 'mongo-raw')
+            col.update({update_id: op_id}, {'$set': update_dic}, upsert=True, multi=True)
+
+        for key in trig_dbs['set']:
+            db_collection = key.split('.')
+            db_name = db_collection[0]
+            collection_name = db_collection[1]
+            set_id = trig_dbs['set'][key]['_id']
+            trig_dbs['set'][key].pop('_id')
+            set_dic = {trig_dbs['set'][key][attr]: doc_set[attr] for attr in trig_dbs['set'][key]}
+            print set_id, ':', op_id,type(op_id)
+            print key, ' $set:', set_dic
+            col = get_mongodb(db_name, collection_name, 'mongo-raw')
+            col.update({set_id: op_id}, {'$set': set_dic}, upsert=True, multi=True)
+
+        for key in trig_dbs['unset']:
+            db_collection = key.split('.')
+            db_name = db_collection[0]
+            collection_name = db_collection[1]
+            unset_id = trig_dbs['unset'][key]['_id']
+            trig_dbs['unset'][key].pop('_id')
+            unset_dic = {trig_dbs['unset'][key][attr]: doc_unset[attr] for attr in trig_dbs['unset'][key]}
+            print unset_id, ':', op_id
+            print key, ' $unset:', unset_dic
+            col = get_mongodb(db_name, collection_name, 'mongo-raw')
+            col.update({unset_id: op_id}, {'$unset': unset_dic}, upsert=True, multi=True)
 
             # update_id = ''
             # if trig_dbs[key][0].keys()[0] == '_id':
@@ -134,8 +174,7 @@ class Trigger(BaseTrigger):
             #         if tmp_dic.keys()[0] == '_id':
             #             update_id = tmp_dic['_id']
             #             break
-            col = get_mongodb(db_name, collection_name, 'mongo-raw')
-            col.update({update_id: op_id}, {'$set': update_dic}, upsert=True, multi=True)
+
             # # 如 提取poi.ViewSpot.country中country
             # prefix_attr = '.'.join(tmp[2:])
             #
